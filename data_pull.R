85213442513b4afaa73261df65346a53ca4fcc93
Robin Browne <robrow@sapient.com>
2017-12-11 21:12:35 +0530

    first draft

--- data_pull.R
+++ data_pull.R
@@ -1,87 +1,96 @@
-## library
-library(tidyverse)
-library(RSiteCatalyst)
-library(jsonlite)
+## load library ----
+  library(tidyverse)
+  library(RSiteCatalyst)
+  library(jsonlite)
 
-## auth
-source("auth.R")
+## auth ----
+  ## the file only has one line calling the SCAuth function from RSiteCatalyst
+  source("auth.R")
 
-## define some metadata
+## define metadata ----
   date_range  <- list(
     "start" = "2017-12-03",
     "end" = "2017-12-09"
   )
+  
+  ## define the report suite to pull data from
   rsid <- "kiamotors-uk-w"
-## define segments ----
-  ## get car model name
-  car_models <- QueueRanked(
+  
+## get ingredients for segment ----
+  ## get car model name in this instance
+  product_id <- QueueRanked(
     reportsuite.id = rsid,
     date.from = date_range$start,
     date.to = date_range$end,
-    metrics = "event2",
+    metrics = "visits",
     elements = "evar7",
     top = 10
   )  
 
 
-## overlap performance
-for (i in 1:(nrow(car_models)-1)) {
-  for (j in (i + 1) : nrow(car_models)) {
-    models <- c(car_models$name[i],car_models$name[j])
-    ## create segment
-    mySegment<- list(container=list(type=unbox("visits"),
-                                    rules = data.frame(
-                                      name = "models",
-                                      element = "evar7",
-                                      operator = "contains",
-                                      value = models
-                                    )))
-    ## get data
-    # df1 <- QueueRanked(
-    #   report.suite = "",
-    #   element = c("geoCity"),
-    #   metrics = "",
-    #   date.from = "",
-    #   date.to = "",
-    #   inline.segment = mySegment)
-    
-    df2 <- QueueOvertime(
-      reportsuite.id =  rsid,
-      date.from = date_range$start,
-      date.to = date_range$end,
-      metrics = "event2",
-      date.granularity = "week",
-      segment.inline =  mySegment)
-    
-    df3 <- df2 %>% select(event2) %>%
-      mutate(
-        model1 = car_models$name[i],
-        model2 = car_models$name[j]
-      ) %>% select(model1,model2,event2)
-    
-    if(i == 1 & j == 2){
-      car_segment_data <- df3
-    }else{
-      car_segment_data <- rbind(car_segment_data,df3)
+## calculate overlap in visits for each model combination
+  for (i in 1:(nrow(product_id)-1)) {
+    for (j in (i + 1) : nrow(product_id)) {
+      models <- c(product_id$name[i],product_id$name[j])
+      ## create segment
+      mySegment<- list(container=list(type=unbox("visits"),
+                                      rules = data.frame(
+                                        name = "models",
+                                        element = "evar7",
+                                        operator = "contains",
+                                        value = product_id
+                                      )))
+      ## get data
+      # df1 <- QueueRanked(
+      #   report.suite = "",
+      #   element = c("geoCity"),
+      #   metrics = "",
+      #   date.from = "",
+      #   date.to = "",
+      #   inline.segment = mySegment)
+      
+      df2 <- QueueOvertime(
+        reportsuite.id =  rsid,
+        date.from = date_range$start,
+        date.to = date_range$end,
+        metrics = "visits",
+        date.granularity = "week",
+        segment.inline =  mySegment)
+      
+      df3 <- df2 %>% select(visits) %>%
+        mutate(
+          model1 = car_models$name[i],
+          model2 = car_models$name[j]
+        ) %>% select(model1,model2,visits)
+      
+      if(i == 1 & j == 2){
+        car_segment_data <- df3
+      }else{
+        car_segment_data <- rbind(car_segment_data,df3)
+      }
+   
     }
- 
+    paste0(i,j)
   }
-}
-  
-write_csv(car_segment_data,"car_segment_data.csv")  
+
+## create a matrix ----
+  ## i am sure there's a better way of doing it
+
+  tmp <- car_segment_data %>%
+    mutate(
+      x = model1,
+      y = model2
+    ) %>%
+    mutate(
+      model1 = y,
+      model2 = x
+    ) %>% select(-x,-y)
+
+  tmp2 <- rbind(car_segment_data,tmp)
 
 ## spread into matrix
-car_matrix <- spread(car_segment_data,model2,event2)
+  car_matrix <- spread(tmp2,model2,visits)
 
 
-tmp <- car_segment_data %>%
-  mutate(
-    x = model1,
-    y = model2
-  ) %>%
-  mutate(
-    model1 = y,
-    model2 = x
-  ) %>% select(-x,-y)
-
-tmp2 <- rbind(car_segment_data,tmp)
+## save the data
+  write_csv(car_matrix,"car_matrix.csv")  
